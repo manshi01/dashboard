889595996819a76a5df2619ebd1c54f592d4950e
unknown <manshibharti9546@gmail.com>
2025-08-21 01:35:07 +0530

    message

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+"use client";
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Dashboard App 🚀</h1>
+      <Button variant="contained" color="primary">
+        Hello MUI
+      </Button>
+    </div>
+  );
+}
